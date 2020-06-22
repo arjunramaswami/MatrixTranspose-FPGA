baea5edeccd8dccb8211861e01d05f23ff2912bf
Arjun Ramaswami <arjunr@mail.upb.de>
2020-06-22 14:59:00 +0200

    updated slurm script

--- scripts/mTrans_opt.sh
+++ scripts/mTrans_opt.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH -A pc2-mitarbeiter
-#SBATCH -J mTrans_opt
+#SBATCH -J cmake_mTrans_opt
 #SBATCH -p fpgasyn
 #SBATCH --mem=90000MB 
 #SBATCH --time=24:00:00
 
 module load intelFPGA_pro/20.1.0 nalla_pcie/19.4.0_hpc
 
-cd ../debug
-rm -rf *
+cd ../build
 
 cmake -DLOG_SIZE=5 ..
 make
